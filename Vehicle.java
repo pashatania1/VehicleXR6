67a3c82b0e435cbce7f331b40c9afdb38e7d3652
Pashatania Fitri Indah Lestari <87978506+pashatania1@users.noreply.github.com>
2025-09-09 09:02:07 +0700

    Add files via upload

---
+++ Vehicle.java
@@ -0,0 +1,19 @@
+public class Vehicle {
+    //property class
+    String merk;
+    int milesPerGallon;
+    
+    //Constructor
+    public Vehicle() {
+        merk = "Toyota";
+        milesPerGallon = 20;
+    }
+
+    public Vehicle(String merk, int milesPerGallon) {
+        this.merk = merk;
+        this.milesPerGallon = milesPerGallon;
+    }
+    //getter
+    //setter
+    //method
+}
